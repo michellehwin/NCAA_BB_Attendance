19210a07b3cb78daabe02307a4fcf7c46e553337
Michelle Nguyen <michellehwin@gmail.com>
2023-01-30 17:17:39 -0600

    attempt plotly

--- TX_WBB_Attendance.py
+++ TX_WBB_Attendance.py
@@ -4,12 +4,69 @@ from bs4 import BeautifulSoup as bs
 import pandas as pd
 import altair as alt
 from cachetools import cached, TTLCache
+import plotly.express as px
+import plotly
+import json
 
 cache = TTLCache(maxsize=100, ttl=86400)
 
 
 @cached(cache)
-def attendance_graph(team_id):
+def plotly_attendance_graph(team_id):
+
+    team_color = "#bf5700"
+    season = 2023
+    url = f"http://www.espn.com/womens-college-basketball/team/schedule/_/id/{team_id}/season/{season}"
+
+    page = rq.get(url)
+    soup = bs(page.content, features="lxml")
+    table = soup.table
+
+    home_game_count = 0
+    home_attendance = 0
+    data = {'Opponent': [],
+            'Attendance': [],
+            'Location': []}
+    # loop through all games played this season
+    for row in table.find_all('tr')[2:]:
+        row_cells = row.find_all('td')
+        try:
+            opponent = row_cells[1].find_all('a')[1].contents[0]
+            game_link = row_cells[2].find('a')['href']
+            if (row_cells[1].find_all('span')[2].contents[-1] == "*"):
+                location = "Neutral"
+            else:
+                location = "Home" if row_cells[1].find_all(
+                    'span')[0].contents[0] == "vs" else "Away"
+
+        except:
+            break
+        game_page = rq.get(game_link)
+        game_soup = bs(game_page.content, features="lxml")
+        atndnce = game_soup.find('div', {"class": "Attendance__Numbers"})
+        atndnce = atndnce.contents[4] if atndnce != None else 0
+        atndnce = ''.join(filter(str.isdigit, atndnce)
+                          ) if atndnce != 0 else atndnce
+        # add current row data to dictionary
+        if atndnce != 0:
+            data["Opponent"].append(opponent)
+            data["Attendance"].append(float(atndnce))
+            data["Location"].append(location)
+            if location == "Home":
+                home_game_count += 1
+                home_attendance += int(atndnce)
+
+    df = pd.DataFrame(data)
+    if (home_game_count > 0):
+        home_attendance = home_attendance/home_game_count  # calculate avg home attendance
+
+    fig = px.bar(df, x='Opponent', y='Attendance')
+    graphJSON = plotly.io.to_json(fig, pretty=True)
+    return graphJSON
+
+
+@cached(cache)
+def attendance_graph(team_id, team_name):
     team_color = "#bf5700"
     season = 2023
     url = f"http://www.espn.com/womens-college-basketball/team/schedule/_/id/{team_id}/season/{season}"
@@ -57,13 +114,16 @@ def attendance_graph(team_id):
         home_attendance = home_attendance/home_game_count  # calculate avg home attendance
 
     chart = alt.Chart(df).mark_bar().encode(
-        x=alt.X('Opponent', sort=data["Opponent"]),
+        x=alt.X('Opponent:O', sort=data["Opponent"]),
         y='Attendance',
         color=alt.Color('Location',
                         scale=alt.Scale(
                             domain=['Home', 'Away', 'Neutral'],
                             range=[team_color, 'darkgray', 'lightgray']))
+    ).properties(
+        title=team_name
     )
     chart_json = chart.to_json()
-    stats = {'chart': chart_json, 'avg_home_attendance': home_attendance}
+    stats = {'attendance_graph': json.loads(chart_json),
+             'avg_home_attendance': home_attendance}
     return stats
